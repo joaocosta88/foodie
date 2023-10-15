4301359cbeefee793575ad522fc83a7cd118710d
Joao Costa <joao.tavares.costa@gmail.com>
2023-10-15 08:14:40 +0100

    working on frontend component

---
+++ web/foodie/src/jsx/App.js
@@ -0,0 +1,16 @@
+import GMap from './renderMap'
+import Header from './header'
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+        <GMap >
+          
+          </GMap>
+    </div>
+  );
+};
+
+export default App;
